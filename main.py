1a5ee5d0294330e00b7de7d8fcf0d4384fff43b0
Cindy Tao <cindy.tao@icwa.wa.gov.au>
2024-02-14 15:02:18 +0800

    change action.yml location

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime 
+import logging
+import os
 
 
 F = open(r'.\result\status.log', 'a')
@@ -6,9 +8,28 @@ F = open(r'.\result\status.log', 'a')
 F.write(f'{datetime.datetime.now()} - The script started running \n')
 
 
-# try:
-#     SOME_SECRET = os.environ["SOME_SECRET"]
-#     logging.info(f"Token value: {SOME_SECRET}")
-# except KeyError:
-#     SOME_SECRET = "Token not available!"
-  
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    "status1.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
+    #logger.info("Token not available!")
+    #raise
+
+
+if __name__ == "__main__":
+
+    logger.info(f"Token value: {SOME_SECRET}")
+    
+    F.write(f'{datetime.datetime.now()} - The script finished running \n')
